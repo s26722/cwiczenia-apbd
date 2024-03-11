03630bfcbb5810183f9b377b4c83319c9cfafb8a
Bartosz Jędras <s26722@pjwstk.edu.pl>
2024-03-11 13:13:10 +0100

    Modyfikacja 2

--- Program1.cs
+++ Program1.cs
@@ -2,5 +2,7 @@ class Program{
     public static void Main(string[] args){
         Console.WriteLine("Hello");
         Console.WriteLine("there");
+        Console.WriteLine("Obi-Wan");
+
     }
 }
