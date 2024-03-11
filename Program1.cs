e796313185257483e31f9840f70a56bc72f6d815
Bartosz Jędras <s26722@pjwstk.edu.pl>
2024-03-11 13:31:46 +0100

    Wersja 1

--- Program1.cs
+++ Program1.cs
@@ -5,5 +5,27 @@ class Program{
         Console.WriteLine("Obi-Wan");
         Console.WriteLine("Kenobi");
 
+
+        
+
+    }
+     static int AverageNum(int [] num){
+
+        int sum = 0;
+        int SumDidgits =0;
+        foreach (int number in num){
+        sum+=number;
+        SumDidgits+=1;
+        }
+        int result = sum/SumDidgits;
+        return result;
+
+
+
+
+
+
+
+
     }
 }
